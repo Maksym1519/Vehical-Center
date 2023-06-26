b443d703314ea676b3ad4d2b83d2afe32aebbbe5
Maksym Korostelov <flying1519@gmail.com>
2023-06-26 21:39:26 +0300

    main page-vhf -in progress

---
+++ src/components/Button.jsx
@@ -0,0 +1,8 @@
+import b from './button.module.scss';
+
+export const Button = () => {
+    return (
+    <div className={b.button}>Connect wallet</div>
+    )
+}
+console.log(Button)
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,19 @@
 //import React from "react";
-//import '../style.css';
+import '../style.scss';
 import h from './header.module.scss';
 import Logo from '../images/header-logo.svg';
+import SearchIcon from '../images/header-search.svg';
+import { Button } from './Button';
 
 export const Header = () => {
     return (
         <div className={h.wrapper}>
           <img src={Logo} alt="logo" className="logo" />
+          <div className={h.search}>
+          <img src= {SearchIcon} alt="search" />
+          <input type="search" className={h.search__input} placeholder='Search for ...'/>
+          </div>
+          <Button />
         </div>
     )
 }
